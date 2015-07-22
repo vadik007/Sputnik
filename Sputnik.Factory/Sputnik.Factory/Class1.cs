f3c632b2e902c2588be774cea10f48cd231a65b9
Vadim Bolshakov <vadim_bolshakov@epam.com>
2015-07-22 11:05:29 +0300

    init

---
+++ Sputnik.Factory/Sputnik.Factory/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sputnik.Factory
+{
+    public class Builder:IBuilder
+    {
+        public IBuilder AddSystem(ISystem system)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IBuilder
+    {
+        IBuilder AddSystem(ISystem system);
+    }
+
+    public interface ISystem
+    {
+        
+    }
+
+    class MyClass
+    {
+        void m()
+        {
+            new Builder()
+                .AddSystem()
+                .AddSystem()
+        }
+    }
+}
